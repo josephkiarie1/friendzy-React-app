ea7f2726737d31abe84128d2951e3e186d9ce4d4
josephkiarie1 <62857999+josephkiarie1@users.noreply.github.com>
2021-11-12 17:54:27 -0800

    added more features on the rightbar

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,8 @@
 import "./post.css";
 import {MoreVert} from "@material-ui/icons";
 
-export default function Post() {
+export default function Post({post}) {
+  console.log(post)
     return (
         <div className="post">
         <div className="postWrapper">
@@ -9,23 +10,23 @@ export default function Post() {
             <div className="postTopLeft"> 
              <img className="postProfileImg" src="/assets/person/1.jpeg" alt=""/>
              <span className="postUserName">Jojo</span>
-             <span className="postDate">5 mins ago</span>
+             <span className="postDate">{post.date}</span>
             </div>
             <div className="postTopRight"></div>
             <MoreVert/>
             </div>
           <div className="postCenter">
-            <span className="postText">Hey! world</span>
-            <img className="postImg" src="/assets/post/3.jpeg" alt=""/>
+            <span className="postText">{post?.desc}</span>
+            <img className="postImg" src={post.photo} alt=""/>
           </div>
           <div className="postBottom">
           <div className="postBottomLeft">
           <img className="likeIcon" src="assets/like.png" alt=""/>
           <img className="likeIcon" src="assets/heart.png" alt=""/>
-          <span className="postLikeCounter">30 and Others liked</span>
+          <span className="postLikeCounter">{post.like} and Others liked it</span>
           </div>
           <div className="postBottomRight">
-          <span className="postCommentText">10 comments</span>
+          <span className="postCommentText">{post.comment}comments</span>
           </div>
           </div>
         </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,66 @@ import "./rightbar.css";
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+           <div className="rightbarWrapper">
+           <div className="birthdayContainer">
+           <img className="birthdayImg" src="assets/gift.png" alt=""/>
+           <span className="birthdayText">
+           <b>Wanguch</b> and <b>3 other friends</b> have birthdays today</span>
+           </div>
+           <img className="rightbarAd" src="assets/ad.png" alt=""/>
+           <h4 className="rightbarTitle">Online friends</h4>
+           <ul className="rightbarFriendList">
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+             <li className="rightbarFriend">
+             <div rightbarProfileImgContainer>
+             <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+             <span className="rightbarOnline"></span>
+             </div>
+             <span className="rightbarUsername">Jojo Wanguch</span>
+             </li>
+           </ul>
+           </div>
         </div>
     )
 }
